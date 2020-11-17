2d523a1b6f12490937f257cee4b213940b7d126b
Filipe <filipe@Filipe-MacBook.local>
2020-11-17 20:14:01 -0300

    feature:reduces

---
+++ aula5-lists/forin/src/reduce.js
@@ -0,0 +1,34 @@
+const {getPeoples} = require('./service/getpeoples')
+
+Array.prototype.meuReduce =  function(callback,valorinicial){
+    let valorFinal = typeof valorinicial !== undefined ? valorinicial : this[0]
+    for(let index = 0 ; index <= this.length -1; index++){
+        valorFinal = callback(valorFinal,this[index],this)
+    }
+    return valorFinal
+}
+
+async function main(){
+    try {
+        const {results} = await getPeoples('a')
+        const pesos = results.map((item)=>{
+             return parseInt(item.height)
+        })
+        console.log('pesos',pesos)
+ /*        const total = pesos.reduce((anterior,proximo) => {
+            return anterior + proximo
+        }) */
+        const minhaLista = [
+            ['Junior','Marcos'],
+            ['Nodebr','Malacabado']
+        ]
+        const total = minhaLista.meuReduce((anterior,proximo) => {
+            return anterior.concat(proximo)
+        },[])
+        .join(', ')
+        console.log('total:',total)
+    } catch (error) {
+        console.error('deu erro', error)
+    }
+}
+main()
