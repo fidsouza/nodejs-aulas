585d39ef88ad1c0d5c4483c37ce7d2caa5517874
Filipe <filipe@Filipe-MacBook.local>
2020-11-20 23:24:49 -0300

    feature:delete registry

--- aula7-cli/src/service/database.js
+++ aula7-cli/src/service/database.js
@@ -1,5 +1,6 @@
 const {
-    readFile
+    readFile,
+    writeFile
 }  = require('fs')
 
 const {
@@ -7,6 +8,7 @@ const {
 } = require('util')
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 
 
@@ -22,13 +24,44 @@ class Database {
         const arquivo = await readFileAsync(this.NOME_ARQUIVO,'utf8')
         return JSON.parse(arquivo.toString())
     }
-    escreverArquivo(){
+    async escreverArquivo(dados){
+        await writeFileAsync(this.NOME_ARQUIVO,JSON.stringify(dados))
+        return true
+    }
 
+    async cadastrar(heroi){
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now()
+        const heroiComId = {
+            id,
+            ...heroi
+        }
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ]
+        const resultado = await this.escreverArquivo(dadosFinal)
+        return resultado
     }
     async listar(id){
         const dados = await this.obterDadosArquivo()
         const dadosFiltrados = dados.filter(item => ( id ? item.id === id : true))
         return dadosFiltrados
+    }
+    async remover(id){
+        if (!id){
+            await this.escreverArquivo([])
+            return true
+        }
+
+        const dados  = await this.obterDadosArquivo()
+        const indice = dados.findIndex((item) => item.id === parseInt(id)) 
+        if(indice === -1){
+            throw Error('O Herói informado não existe')
+        }
+        dados.splice(indice,1)
+        return await this.escreverArquivo(dados)
+
     }
 }
 
--- aula7-cli/src/test/test.js
+++ aula7-cli/src/test/test.js
@@ -9,9 +9,25 @@ const DEFAULT_ITEM_CADASTRADO = {
 const database = require('../service/database')
 
 describe('Suite de manipulação de Heróis',()=>{
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRADO)
+    })
     it('deve pesquisar um herói, usando arquivos.', async () => {
         const expected = DEFAULT_ITEM_CADASTRADO
         const [resultado] = await database.listar(expected.id)
+
+        deepEqual(resultado,expected)
+    })
+    it('deve cadastrar um herói, usando arquivos.', async () => {
+        const expected  = DEFAULT_ITEM_CADASTRADO
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRADO)
+        const [actual]  = await database.listar(DEFAULT_ITEM_CADASTRADO.id)
+
+        deepEqual(actual,expected)
+    })
+    it('deve remover um herói por id, usando arquivos.', async () => {
+        const expected  = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRADO.id)
 
         deepEqual(resultado,expected)
     })
