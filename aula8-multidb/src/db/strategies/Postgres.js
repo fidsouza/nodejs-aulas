74a121a2daf9e04a4568a529c4cdadf54cd75aba
Filipe <filipe@Filipe-MacBook.local>
2020-11-24 01:01:39 -0300

    feature:object-keys

--- aula8-multidb/src/db/strategies/Postgres.js
+++ aula8-multidb/src/db/strategies/Postgres.js
@@ -1,8 +1,55 @@
 const ICrud = require('./interface/ICrud')
+const Sequelize = require('sequilize')
 
 class Postgres extends ICrud{
     constructor(){
         super()
+        this._driver = null
+        this._herois = null
+        this._connect()
+    }
+    async isConnected(){
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.error('Erro inesperado:',error)
+        }
+    }
+    defineModel(){
+         this._herois = _driver.define('herois',{
+            id:{
+                type:Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome:{
+                type:Sequelize.STRING,
+                required:true
+    
+            },
+            poder:{
+                type:Sequelize.STRING,
+                required:true
+            }
+    
+        })
+        await Herois.sync()
+    }
+    _connect(){
+        this._driver = new Sequelize(
+            'herois',  //database
+            'fidsouza',//user
+            'minhasenhanova', //password
+            {
+                host:'localhost',
+                dialect:'postgres',
+                quoteIdentifiers:false,
+                operatorAliases:false
+            }        
+        )
+
     }
     create(item){
         console.log('cadastrado no postgres')
