2f670bcd7ef1404426d88d8c48260c48abebc5a0
Filipe <filipe@Filipe-MacBook.local>
2020-12-08 16:11:44 -0300

    feature:structure hapips and routes

---
+++ aula12-api/src/api.js
@@ -0,0 +1,32 @@
+const Hapi = require('hapi')
+const HeroRoutes = require('../src/routes/heroesRoutes')
+
+const Context  = require('./db/strategies/base/ContextStratagy')
+const Postgres = require('./db/strategies/postgres/Postgres')
+const HeroiSchema = require('./db/strategies/postgres/schemas/heroiSchema')
+
+let context = {}
+
+const app  = new Hapi.Server({
+    port:5000
+})
+
+function mapRoutes(instance,methods){
+    return methods.map(method => instance[method]())
+}
+
+async function main(){
+
+    const connection = await Postgres.connect()
+    const model = await Postgres.defineModel(connection,HeroiSchema)
+    context = new Context(new Postgres(connection,model))
+
+    app.route([
+        ...mapRoutes(new HeroRoutes(context),HeroRoutes.methods())
+    ])
+    await app.start()
+    console.log('api ok')
+    return app
+}
+
+module.exports = main()
---
+++ aula12-api/src/db/strategies/postgres/Postgres.js
@@ -0,0 +1,85 @@
+const ICrud = require('../interface/ICrud')
+const Sequelize = require('sequelize')
+
+
+class Postgres extends ICrud{
+    constructor(connection,schema){
+        super()
+        this._connection = connection
+        this._schema = schema
+    }
+    async isConnected(){
+        try {
+            await this._connection.authenticate()
+            return true
+        } catch (error) {
+            console.error('Erro inesperado:',error)
+        }
+    }
+   static async defineModel(connection,schema){
+        const model = connection.define(
+            schema.name,schema.schema
+        )
+        await model.sync()
+        return model
+    }
+    async create(item){
+        try {
+            const {dataValues:{
+                nome,
+                poder
+            }} = await this._schema.create(item)
+            return {nome,poder}
+        } catch (error) {
+            console.error('Error Inesperado ao Cadastrar',error)
+        }
+    }
+    async read(item ={}){
+        try {
+            const result =  await this._schema.findAll({where:item,raw:true})
+            return result
+        } catch (error) {
+            console.error('Erro ao realizar a consulta',error)
+        }
+    }
+    async update(id,item){
+        try {
+            const result =  await this._schema.update(item,{where:{id:id},returning:true})
+            return result[1][0].get()
+        } catch (error) {
+            console.error('Erro ao atualizar',error)
+        }
+    }
+    async delete(id){
+        try {
+            const query = id ? {id}  : console.error('ID para deleção Inválido')
+            return await this._schema.destroy({where:query})
+
+        } catch (error) {
+            console.error('Erro inesperado ao deletar',error)
+        }
+    }
+   static async connect(){
+      const connection =   this._connection    = new Sequelize(
+             'heroes',  //database
+             'fidsouza',//user
+             'minhasenhanova', //password
+             {
+                 host:'localhost',
+                 dialect:'postgres',
+                 quoteIdentifiers:false,
+                 operatorAliases:false,
+                 logging:false
+                 
+             }
+         
+         )
+      console.log('database ok')
+      return connection
+     }
+
+
+}
+
+
+module.exports = Postgres
